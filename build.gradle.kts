6d356db6cb7bfadf8cd8cb364e9cb9e41e30a666
Will FP <william.favierparsons1@gmail.com>
2023-12-07 17:25:47 +0000

    libreforge-updater

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     `java-library`
     `maven-publish`
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.9.20"
     id("com.github.johnrengelman.shadow") version "8.0.0"
     id("com.willfp.libreforge-gradle-plugin") version "1.0.0"
 }
@@ -39,7 +39,7 @@ allprojects {
     dependencies {
         compileOnly("com.willfp:eco:6.65.4")
         compileOnly("org.jetbrains:annotations:23.0.0")
-        compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+        compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
 
         compileOnly("com.github.ben-manes.caffeine:caffeine:3.1.0")
         implementation("com.willfp:ecomponent:1.4.1")
