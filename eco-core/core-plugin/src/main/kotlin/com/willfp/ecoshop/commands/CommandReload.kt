df8631177207174cff3e9d06124687997f8b9726
NicoNeko <nicuch100@gmail.com>
2026-04-24 22:47:44 +0300

    Add folia support (#19)
    Co-authored-by: Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoshop/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoshop/commands/CommandReload.kt
@@ -1,5 +1,6 @@
 package com.willfp.ecoshop.commands
 
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
@@ -8,18 +9,24 @@ import com.willfp.ecoshop.shop.ShopCategories
 import com.willfp.ecoshop.shop.Shops
 import org.bukkit.command.CommandSender
 
-object CommandReload: Subcommand(
+object CommandReload : Subcommand(
     plugin,
     "reload",
     "ecoshop.command.reload",
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        sender.sendMessage(
-            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%time%", plugin.reloadWithTime().toNiceString())
-                .replace("%shops%", Shops.values().size.toString())
-                .replace("%categories%", ShopCategories.values().size.toString())
-        )
+        val runnable = Runnable {
+            sender.sendMessage(
+                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                    .replace("%time%", plugin.reloadWithTime().toNiceString())
+                    .replace("%shops%", Shops.values().size.toString())
+                    .replace("%categories%", ShopCategories.values().size.toString())
+            )
+        }
+        if (Prerequisite.HAS_FOLIA.isMet)
+            plugin.scheduler.runTask(runnable) // run on global thread
+        else
+            runnable.run()
     }
 }
