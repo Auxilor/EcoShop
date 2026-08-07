1f9abada63cbd7b1f3545df6d3095e178897ea85
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-08-07 16:22:43 +0100

    fix: Publish

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -6,31 +6,3 @@ dependencies {
 
     implementation("com.willfp:ecomponent:1.5.0")
 }
-
-tasks {
-    build {
-        dependsOn(publishToMavenLocal)
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("shadow") {
-            from(components["java"])
-            artifactId = rootProject.name
-        }
-    }
-
-    publishing {
-        repositories {
-            maven {
-                name = "Auxilor"
-                url = uri("https://repo.auxilor.io/repository/maven-releases/")
-                credentials {
-                    username = System.getenv("MAVEN_USERNAME")
-                    password = System.getenv("MAVEN_PASSWORD")
-                }
-            }
-        }
-    }
-}
